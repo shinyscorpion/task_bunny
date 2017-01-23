2d967883497c6696d1edae3659bdae1184fc6dc2
Ian Luites <IanLuites@users.noreply.github.com>
2017-01-23 09:40:28 +0000

    Add ExDoc to build documentation.

--- mix.exs
+++ mix.exs
@@ -2,13 +2,25 @@ defmodule TaskBunny.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :task_bunny,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :task_bunny,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      elixirc_paths: elixirc_paths(Mix.env),
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      # Docs
+      name: "TaskBunny",
+      source_url: "https://bitbucket.org/squareenixoid/onlinedev-task-bunny/",
+      docs:
+        [
+          extras:
+            [
+              "README.md",
+            ],
+        ],
+    ]
   end
 
   # Configuration for the OTP application
@@ -37,7 +49,8 @@ defmodule TaskBunny.Mixfile do
       {:amqp_client, github: "jbrisbin/amqp_client", override: true, ref: "d50aec00b94f0766a048b4eceaf25ddfdeeb1d86"},
       {:rabbit_common, github: "jbrisbin/rabbit_common", override: true, ref: "80814606ae23cc820c74e443383e192cd69ec030"},
       {:meck, "~> 0.8.2", only: :test},
-      {:logger_file_backend, "~> 0.0.9", only: :test}
+      {:logger_file_backend, "~> 0.0.9", only: :test},
+      {:ex_doc, "~> 0.14", only: :dev},
     ]
   end
 end
