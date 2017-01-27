e855a11f2d22d595dccb484da21c01b1db2d13d6
Ian Luites <IanLuites@users.noreply.github.com>
2017-01-27 14:49:03 +0000

    Add documentation to WorkerSupervisor.

--- lib/task_bunny/worker_supervisor.ex
+++ lib/task_bunny/worker_supervisor.ex
@@ -1,11 +1,21 @@
 defmodule TaskBunny.WorkerSupervisor do
+  @moduledoc """
+  Worker supervisor for TaskBunny.
+
+  It supervises all Workers with one_for_one strategy.
+
+  It will receive all jobs that need workers when started and will start a worker for each job.
+  """
+
   use Supervisor
+
   alias TaskBunny.Worker
 
   def start_link(jobs) do
     Supervisor.start_link(__MODULE__, jobs)
   end
 
+  @spec init(list({host :: atom, job :: atom, concurrenct :: integer})) :: {:ok, {:supervisor.sup_flags, [Supervisor.Spec.spec]}} | :ignore
   def init(jobs) do
     jobs
     |> Enum.map(fn ({host, job, concurrency}) ->
