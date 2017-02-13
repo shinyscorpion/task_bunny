8360140a6f7810bfa00424e8a730c5a18d85aa41
Ian Luites <IanLuites@users.noreply.github.com>
2017-02-13 14:20:41 +0000

    Add wobserver registration on startup.

--- lib/task_bunny.ex
+++ lib/task_bunny.ex
@@ -5,8 +5,12 @@ defmodule TaskBunny do
 
   use Application
 
+  alias TaskBunny.Status
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
+
+    register_metrics()
 
     # Define workers and child supervisors to be supervised
     children = [
@@ -15,5 +19,12 @@ defmodule TaskBunny do
 
     opts = [strategy: :one_for_one, name: TaskBunny]
     Supervisor.start_link(children, opts)
+  end
+
+  defp register_metrics do
+    if Code.ensure_loaded(Wobserver) == {:module, Wobserver} do
+      Wobserver.register :page, {"Task Bunny", :taskbunny, &Status.page/0}
+      Wobserver.register :metric, [&Status.metrics/0]
+    end
   end
 end
