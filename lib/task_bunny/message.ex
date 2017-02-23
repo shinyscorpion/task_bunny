a60be8f984c91fe26386983cb521f0a0e5531666
Tatsuya Ono <ono@users.noreply.github.com>
2017-02-23 10:12:39 +0000

    Add an error log to message and remove the logic to count error from message meta data

--- lib/task_bunny/message.ex
+++ lib/task_bunny/message.ex
@@ -13,7 +13,7 @@ defmodule TaskBunny.Message do
       "payload" => payload,
       "created_at" => DateTime.utc_now()
     }
-    |> Poison.encode!
+    |> Poison.encode!(pretty: true)
   end
 
   @doc """
@@ -59,80 +59,25 @@ defmodule TaskBunny.Message do
   end
 
   @doc """
-  Retrieves number of count the message was consumed and failed to process
-
-  ## Example
-
-      iex> meta = %{app_id: :undefined, cluster_id: :undefined, consumer_tag: "amq.ctag-6gbrfVhVEsg5UluIEagNcQ", content_encoding: :undefined, content_type: :undefined, correlation_id: :undefined, delivery_tag: 69, exchange: "", expiration: :undefined, headers: [{"x-death", :array, [table: [{"count", :long, 67}, {"exchange", :longstr, ""}, {"queue", :longstr, "dlx.retry"}, {"reason", :longstr, "expired"}, {"routing-keys", :array, [longstr: "dlx.retry"]}, {"time", :timestamp, 1484651945}], table: [{"count", :long, 67}, {"exchange", :longstr, ""},{"queue", :longstr, "dlx"}, {"reason", :longstr, "rejected"}, {"routing-keys", :array, [longstr: "dlx"]}, {"time", :timestamp, 1484651915}]]}], message_id: :undefined, persistent: true, priority: :undefined, redelivered: false, reply_to: :undefined, routing_key: "dlx",timestamp: :undefined, type: :undefined, user_id: :undefined}
-      iex> TaskBunny.Message.failed_count(meta)
-      67
+  Add an error log to message body.
   """
-  @spec failed_count(map | tuple | any) :: integer
-  def failed_count(meta)
-
-  def failed_count(%{headers: :undefined}), do: 0
-
-  def failed_count(%{headers: headers}) do
-    x_death = Enum.find headers, fn ({key, _, _}) ->
-      key == "x-death"
-    end
-
-    failed_count(x_death)
+  @spec add_error_log(String.t|map, any) :: String.t | map
+  def add_error_log(message, error) when is_map(message) do
+    error = %{
+      "result" => inspect(error),
+      "failed_at" => DateTime.utc_now(),
+      "pid" => inspect(self())
+    }
+    errors = (message["errors"] || []) |> List.insert_at(-1, error)
+    Map.merge(message, %{"errors" => errors})
   end
 
-  def failed_count({"x-death", :array, tables}) do
-    count =
-      tables
-      |> Enum.map(fn({_, attributes}) ->
-           count_attr = Enum.find attributes, fn ({key, _, _}) ->
-             key == "count"
-           end
-
-           case count_attr do
-             {_, _, count} -> count
-             _ -> 0
-           end
-         end)
-      |> Enum.max(fn -> 0 end)
-
-    if count > 0, do: count, else: failed_count_pre_3_6(tables)
+  def add_error_log(raw_message, error) do
+    raw_message
+    |> Poison.decode!()
+    |> add_error_log(error)
+    |> Poison.encode!(pretty: true)
   end
 
   def failed_count(_), do: 0
-
-  # Priort to 3.6, it doesn't contain count information.
-  # We need to count it up by ourselves.
-  @spec failed_count_pre_3_6(list) :: integer
-  defp failed_count_pre_3_6(tables) do
-    # List up queues
-    queues =
-      tables
-      |> Enum.map(fn ({_, tuples}) ->
-        tuple = Enum.find(tuples, fn ({key, _type, _value}) ->
-          key == "queue"
-        end)
-        case tuple do
-          {_, _, queue_name} -> queue_name
-          _ -> nil
-        end
-      end)
-      |> Enum.filter(fn (queue) -> queue end)
-
-    # Count up queues
-    # ["jobs.a.retry", "jobs.a", "jobs.a.retry", "jobs.a"]
-    # => %{"jobs.a.retry" => 2, "jobs.a" => 2}
-    # => then takes the max value
-
-    queues
-    |> Enum.reduce(%{}, fn (queue, counts) ->
-      if counts[queue] do
-        %{counts | queue => counts[queue] + 1}
-      else
-        Map.merge(counts, %{queue => 1})
-      end
-    end)
-    |> Map.to_list
-    |> Enum.map(fn ({_q, count}) -> count end)
-    |> Enum.max
-  end
 end
--- test/task_bunny/message_test.exs
+++ test/task_bunny/message_test.exs
@@ -36,19 +36,15 @@ defmodule TaskBunny.MessageTest do
     end
   end
 
-  describe "failed_count" do
-    test "RabbitMQ 3.6 header" do
-      meta = %{app_id: :undefined, cluster_id: :undefined, consumer_tag: "amq.ctag-6gbrfVhVEsg5UluIEagNcQ", content_encoding: :undefined, content_type: :undefined, correlation_id: :undefined, delivery_tag: 69, exchange: "", expiration: :undefined, headers: [{"x-death", :array, [table: [{"count", :long, 67}, {"exchange", :longstr, ""}, {"queue", :longstr, "dlx.retry"}, {"reason", :longstr, "expired"}, {"routing-keys", :array, [longstr: "dlx.retry"]}, {"time", :timestamp, 1484651945}], table: [{"count", :long, 67}, {"exchange", :longstr, ""},{"queue", :longstr, "dlx"}, {"reason", :longstr, "rejected"}, {"routing-keys", :array, [longstr: "dlx"]}, {"time", :timestamp, 1484651915}]]}], message_id: :undefined, persistent: true, priority: :undefined, redelivered: false, reply_to: :undefined, routing_key: "dlx",timestamp: :undefined, type: :undefined, user_id: :undefined}
+  describe "add_error_log" do
+    @tag timeout: 1000
+    test "adds error information to the message" do
+      message = Message.encode(NameJob, %{"name" => "Joe"})
+      error = {:error, "HTTP Request error"}
+      new_message = Message.add_error_log(message, error)
+      {:ok, %{"errors" => [added | _]}} = Message.decode(new_message)
 
-      assert Message.failed_count(meta) == 67
-    end
-
-    test "RabbitMQ 3.4 header" do
-      headers = [{"x-death", :array, [table: [{"reason", :longstr, "expired"}, {"queue", :longstr, "jobs.test_job.retry"}, {"time", :timestamp, 1487256438}, {"exchange", :longstr, ""}, {"routing-keys", :array, [longstr: "jobs.test_job.retry"]}], table: [{"reason", :longstr, "rejected"}, {"queue", :longstr, "jobs.test_job"}, {"time", :timestamp, 1487256438}, {"exchange", :longstr, ""}, {"routing-keys", :array, [longstr: "jobs.test_job"]}], table: [{"reason", :longstr, "expired"}, {"queue", :longstr, "jobs.test_job.retry"}, {"time", :timestamp, 1487256438}, {"exchange", :longstr, ""}, {"routing-keys", :array, [longstr: "jobs.test_job.retry"]}], table: [{"reason", :longstr, "rejected"}, {"queue", :longstr, "jobs.test_job"}, {"time", :timestamp, 1487256438}, {"exchange", :longstr, ""}, {"routing-keys", :array, [longstr: "jobs.test_job"]}], table: [{"reason", :longstr, "expired"}, {"queue", :longstr, "jobs.test_job.retry"}, {"time", :timestamp, 1487256438}, {"exchange", :longstr, ""}, {"routing-keys", :array, [longstr: "jobs.test_job.retry"]}], table: [{"reason", :longstr, "rejected"}, {"queue", :longstr, "jobs.test_job"}, {"time", :timestamp, 1487256438}, {"exchange", :longstr, ""}, {"routing-keys", :array, [longstr: "jobs.test_job"]}]]}]
-
-      meta = %{app_id: :undefined, cluster_id: :undefined, consumer_tag: "amq.ctag-6gbrfVhVEsg5UluIEagNcQ", content_encoding: :undefined, content_type: :undefined, correlation_id: :undefined, delivery_tag: 69, exchange: "", expiration: :undefined, headers: headers, message_id: :undefined, persistent: true, priority: :undefined, redelivered: false, reply_to: :undefined, routing_key: "dlx",timestamp: :undefined, type: :undefined, user_id: :undefined}
-
-      assert Message.failed_count(meta) == 3
+      assert added["result"] == inspect(error)
     end
   end
 end
