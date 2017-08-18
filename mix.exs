eca5ba384d047c31f360e51c126a95a918a24cf4
Ricardo Perez <ricardop@square-enix.com>
2017-08-18 16:10:31 +0100

    Bump version to 0.2.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule TaskBunny.Mixfile do
   use Mix.Project
 
-  @version "0.2.1"
+  @version "0.2.2"
   @description "Background processing application/library written in Elixir that uses RabbitMQ as a messaging backend"
 
   def project do
